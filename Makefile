970bde776f94b8e150d3e62f68665ad881b41520
404salad <404salad@gmail.com>
2023-12-15 02:05:55 +0530

    gui working

--- Makefile
+++ Makefile
@@ -1,2 +1,3 @@
 main: main.c
-	gcc main.c -o main
+	gcc main.c -o main $$(pkg-config --cflags --libs gtk+-3.0)
+
--- main.c
+++ main.c
@@ -1,10 +1,15 @@
+#include <gtk/gtk.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/errno.h>
 #include <string.h>
 
-int main() {
-    MessageBoxA(NULL, "Message", "Title", MB_OK);
+void on_button_clicked(GtkWidget *widget, gpointer data) {
+    g_print("exit on button press\n");
+    exit(0);
+}
+
+int main(int argc, char* argv) {
     FILE *fp = popen("bash check.sh", "r");
     if (fp == NULL) {
         perror("popen error");
@@ -24,13 +29,25 @@ int main() {
     }
 
     int battery = atoi(percent);
+    battery = 13;
     if(battery>20) {
         puts("suffient battery");
+        exit(EXIT_SUCCESS);
     }
     else {
         puts("charge now");
+        gtk_init(&argc, &argv);
+        GtkWidget *window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+        gtk_window_set_title(GTK_WINDOW(window), "battery alert");
+        gtk_container_set_border_width(GTK_CONTAINER(window), 100);
+        GtkWidget *button = gtk_button_new_with_label("BATTERY LOW");
+        // callback function
+        g_signal_connect(button, "clicked", G_CALLBACK(on_button_clicked), NULL);
+        gtk_container_add(GTK_CONTAINER(window), button);
+        g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
+        gtk_widget_show_all(window);
+        gtk_main();
     }
-
 
     return 0;
 }
