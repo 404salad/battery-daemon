1386ebcb875cb510a79d9b6453d424562e00ef92
sahil <110289934+404salad@users.noreply.github.com>
2023-12-15 02:39:28 +0530

    cleanup

--- check.sh
+++ check.sh
@@ -2,9 +2,3 @@
 bat=$(acpi | grep -oE "[0-9]{1,}"%)
 bat=${bat%?} #strip the last char which is % 
 echo $bat
-
-#   if [ $bat -le 20 ]; then
-#       echo "battery low"
-#   else
-#       echo "sufficient battery"
-#   fi
